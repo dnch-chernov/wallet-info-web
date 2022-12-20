cd26fc3015ca7915ab7550f29eca343765f1c93d
Denis Chernov <gitlab@dnch.me>
2022-12-19 22:32:58 -0800

    skip error handling for now

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -5,27 +5,21 @@ import { Grid, Header } from 'semantic-ui-react';
 export function ResultsPage() {
   const { token, address } = useParams();
   const [balance, setBalance] = useState("");
-  const [error, setError] = useState("");
   useEffect(() => {
     fetch(`/api/balance/${token}/${address}`)
       .then(res => res.json())
       .then(
         (result) => {
           setBalance(`${result.balance} ${token.toLocaleUpperCase()}`);
-        },
-        (error) => {
-          setError(error);
         }
       )
   }, [token, address]);
   return (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      {error ? <div>{error}</div> :
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as='h2' color='teal' data-testid='header-title'>Balance</Header>
-          <Header as='h2' color='grey' data-testid='header-value'>{balance}</Header>
-        </Grid.Column>
-      }
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' data-testid='header-title'>Balance</Header>
+        <Header as='h2' color='grey' data-testid='header-value'>{balance}</Header>
+      </Grid.Column>
     </Grid>
   );
 }
