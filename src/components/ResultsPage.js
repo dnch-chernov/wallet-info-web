fc970fdbbb8202d4be9772fee5135224474fe4f6
Denis Chernov <gitlab@dnch.me>
2022-12-19 22:21:02 -0800

    use backend

--- src/components/ResultsPage.js
+++ src/components/ResultsPage.js
@@ -2,56 +2,28 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { Grid, Header } from 'semantic-ui-react';
 
-const Web3 = require("web3");
-const w3 = new Web3("https://goerli.infura.io/v3/606f8736b7d14795a3cba86cd814d4fa");
-const usdcContractAddress = '0x07865c6e87b9f70255377e024ace6630c1eaa37f';
-const minABI = [
-    // balanceOf
-    {
-        "constant": true,
-        "inputs": [{ "name": "_owner", "type": "address" }],
-        "name": "balanceOf",
-        "outputs": [{ "name": "balance", "type": "uint256" }],
-        "type": "function"
-    },
-    // decimals
-    {
-        "constant": true,
-        "inputs": [],
-        "name": "decimals",
-        "outputs": [{ "name": "", "type": "uint8" }],
-        "type": "function"
-    }
-];
-const contractUSDC = new w3.eth.Contract(minABI, usdcContractAddress);
-
 export function ResultsPage() {
-    const { token, address } = useParams();
-    const [balance, setBalance] = useState("");
-    useEffect(() => {
-        switch (token.toLocaleLowerCase()) {
-            case 'eth':
-                w3.eth.getBalance(address, (err, wei) => {
-                    setBalance(`${w3.utils.fromWei(wei, 'ether')} ${token.toLocaleUpperCase()}`);
-                });
-                break;
-            case 'usdc':
-                contractUSDC.methods.balanceOf(address).call().then((wei) => {
-                    setBalance(`${w3.utils.fromWei(wei, 'mwei')} ${token.toLocaleUpperCase()}`);
-                });
-                break;
-            default:
-                setBalance('-1')
-
-
+  const { token, address } = useParams();
+  const [balance, setBalance] = useState("");
+  const [error, setError] = useState("");
+  useEffect(() => {
+    fetch(`/api/balance/${token}/${address}`)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setBalance(`${result.balance} ${token.toLocaleUpperCase()}`);
+        },
+        (error) => {
+          setError(error);
         }
-    }, [token, address]);
-    return (
-        <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-            <Grid.Column style={{ maxWidth: 450 }}>
-                <Header as='h2' color='teal' data-testid='header-title'>Balance</Header>
-                <Header as='h2' color='grey' data-testid='header-value'>{balance}</Header>
-            </Grid.Column>
-        </Grid>
-    );
+      )
+  }, [token, address]);
+  return (
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as='h2' color='teal' data-testid='header-title'>Balance</Header>
+        <Header as='h2' color='grey' data-testid='header-value'>{balance}</Header>
+      </Grid.Column>
+    </Grid>
+  );
 }
